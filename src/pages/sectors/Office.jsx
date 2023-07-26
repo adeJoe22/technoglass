d347fca662a965556c65948fc46d0651084d2d14
Apatakayuss <apatakayuss@gmail.com>
2023-07-26 19:10:11 +0100

    office started

--- src/pages/sectors/Office.jsx
+++ src/pages/sectors/Office.jsx
@@ -1,7 +1,67 @@
 import React from 'react'
+import {styled} from 'styled-components'
+import InnerHero from '../../components/InnerHero'
+import Card from '../../components/Card'
+import Background from '../../assets/background.jpg'
 
-export default function Office() {
+export default function Facility() {
   return (
-    <div>Office</div>
+    <OfficeWrapper>
+         <InnerHero
+          backgroundImage={Background}
+          text="Facility"  
+     />
+
+     <MidSection>
+      <Left>
+        <ImageContainer>
+          <Image src={Background}/>
+        </ImageContainer>
+      </Left>
+
+      <Right>
+        <Text>
+        Creating Inspirational workspace environments requires an inspirational supply partner. Our extensive experience in decorative glass provides our clients with a wealth of opportunity to create truly stunning interior spaces. From high quality, painted glass products to specialist laminates incorporating decorative fabrics, Mi Glass can bring any project to vivid life.
+        </Text>
+
+        <Text>
+        Our manufacturing capabilities include state of the art vertical and horizontal CNC processing, in-house laminating and screen printing. Mi Glass can also offer digital printing which combines the durability of glass with the latest in hi-resolution print technology to create vibrant images providing the perfect backdrop for any commercial environment.
+        </Text>
+
+        <Text>
+          Our services include:
+        </Text>
+      </Right>
+     </MidSection>
+
+     <Card
+    Header="Reflecting our customers need."
+    text="Friendly, competitive & service driven"
+    button1Text="Click here"
+    button2Text="Click me"
+    />
+    </OfficeWrapper>
   )
 }
+
+
+const OfficeWrapper = styled.div``
+const MidSection = styled.div`
+  padding: 50px 70px;
+`
+const Text = styled.p`
+  margin-bottom: 20px;
+  font-size: 14px;
+`
+const Left = styled.div`
+ 
+`
+const Right = styled.div`
+  
+`
+const ImageContainer = styled.div`
+  
+`
+const Image = styled.img`
+  
+`
