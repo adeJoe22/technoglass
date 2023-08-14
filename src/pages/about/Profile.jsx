b0cec0d5f39e09bb4bc60aa2f6f061563684beae
thomasAmara <thomastarawali@gmail.com>
2023-08-14 18:03:44 +0100

    swiper

--- src/pages/about/Profile.jsx
+++ src/pages/about/Profile.jsx
@@ -1,11 +1,41 @@
-import { Box, Text } from '@chakra-ui/react';
+import { Box, Image, Text } from '@chakra-ui/react';
 import React from 'react';
 import { styled } from 'styled-components';
 import InnerHero from '../../components/InnerHero';
-import Background from '../../assets/background.jpg';
+import Background from '../../assets/CH13.jpg';
 import Card from '../../components/Card';
+import Ocean from '../../assets/OceanParade.svg';
+import Ocean2 from '../../assets/OceanParade2.svg';
+import Ocean3 from '../../assets/coscharis.JPG';
+import Ocean4 from '../../assets/OceanParade4.png';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import '../../styles/swiper.css';
 
 export default function Profile() {
+  const Data = [
+    {
+      image: Ocean,
+      title: 'Glass FACADE',
+      desc: 'Ocean Parade Estate, Banana island, Lagos',
+    },
+    {
+      image: Ocean3,
+      title: 'Glass Curtain Wall And Partitioning',
+      desc: 'Murtala Muhammed International Airport, Ikeja, Lagos',
+    },
+    {
+      image: Ocean2,
+      title: 'Glass Curtain Wall',
+      desc: 'Coscharis Motors Lekki, Lagos',
+    },
+    {
+      image: Ocean3,
+      title: 'Glass Curtain Wall And Partitioning',
+      desc: 'Murtala Muhammed International Airport, Ikeja, Lagos',
+    },
+  ];
+
   return (
     <Container>
       <InnerHero backgroundImage={Background} text='Company Profile' />
@@ -58,18 +88,152 @@ export default function Profile() {
           Welcome to Technoglass – where innovation meets vision.
         </Text>
       </Box>
-      {/* <Box maxWidth='1280px' p='25px' margin='0 auto'>
-        <Box textAlign='center'>
-          <Text mt='10px' fontWeight='700' fontSize='32px'>
-            Reflecting our customer needs.
-          </Text>
-        </Box>
-        <Box textAlign='center'>
-          <Text mt='10px' fontWeight='400' fontSize='16px'>
-            Friendly, competitive & service driven
-          </Text>
-        </Box>
-      </Box> */}
+      <Box maxWidth='1280px' p='25px' margin='0 auto'>
+        <Text
+          textAlign='center'
+          textTransform='capitalize'
+          fontSize='24px'
+          letterSpacing='0.24px'
+          fontWeight='600'
+          color='#B7CC33'
+        >
+          Featured Projects
+        </Text>
+      </Box>
+      <Box maxWidth='1280px' margin='0 auto' px='30px'>
+        <Swiper
+          navigation={true}
+          style={{
+            '--swiper-pagination-color': '#004332',
+            '--swiper-navigation-color': '#004332',
+            'navigation-button': '4px',
+          }}
+          modules={[Navigation, Pagination, Scrollbar, A11y]}
+          spaceBetween={5}
+          slidesPerView='3'
+          direction='horizontal'
+          pagination={true}
+        >
+          {Data.map((item, i) => {
+            return (
+              <SwiperSlide>
+                <Box
+                  // className='BgBox2'
+                  //   display='flex'
+                  //   borderColor='#000'
+                  //   maxWidth='350px'
+                  //   height='200px'
+                  //   width='100%'
+                  //   boxShadow='0px 0px 3px rgba(0 , 0 , 0, 0.3)'
+                  style={{
+                    display: 'flex',
+                    maxWidth: '350px',
+                    borderRadius: '12px',
+                    border: '2px',
+                    height: '200px',
+                    padding: '0px',
+                    backgroundPosition: 'center',
+                    backgroundImage: `url(${item.image})`,
+                  }}
+                ></Box>
+                <Box px='5px' width='90%'>
+                  <Text
+                    textAlign='center'
+                    fontSize='18px'
+                    letterSpacing='0.24px'
+                    fontWeight='600'
+                    textTransform='capitalize'
+                    color='#B7CC33'
+                  >
+                    {item.title}
+                  </Text>
+                  <Text textAlign='center' fontWeight='500' fontSize='14px'>
+                    {item.desc}
+                  </Text>
+                </Box>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      </Box>
+      <Box maxWidth='1280px' p='25px' margin='0 auto'>
+        {/* <Box
+          display='flex'
+          mb='20px'
+          justifyContent='space-between'
+          flexDirection='row'
+        >
+          <Box>
+            <Image src={Ocean} />
+            <Text
+              fontSize='24px'
+              letterSpacing='0.24px'
+              fontWeight='600'
+              textTransform='capitalize'
+              color='#B7CC33'
+            >
+              Glass FaCADE
+            </Text>
+            <Text fontSize='16px' fontWeight='500'>
+              Ocean Parade Estate, Banana island, Lagos
+            </Text>
+          </Box>
+          <Box>
+            <Image src={Ocean2} />
+            <Text
+              fontSize='24px'
+              letterSpacing='0.24px'
+              fontWeight='600'
+              textTransform='capitalize'
+              color='#B7CC33'
+            >
+              Glass CURTAIN WALL
+            </Text>
+            <Text fontSize='16px' fontWeight='500'>
+              Murtala Muhammed International Airport Ikeja, Lagos
+            </Text>
+          </Box>
+        </Box> */}
+        {/* <Box
+          display='flex'
+          mt='20px'
+          justifyContent='space-between'
+          flexDirection='row'
+        >
+          <Box display='flex' flexDirection='row'>
+            <Box
+              width='40%'
+              display='flex'
+              flexDirection='column'
+              alignItems='center'
+            >
+              <Text
+                fontSize='24px'
+                letterSpacing='0.24px'
+                fontWeight='600'
+                color='#B7CC33'
+              >
+                Glass CURTAIN WALL
+              </Text>
+              <Text fontSize='16px' fontWeight='500'>
+                Murtala Muhammed International Airport Ikeja, Lagos
+              </Text>
+            </Box>
+            <Image src={Ocean3} />
+          </Box>{' '}
+          <Box display='flex' width='50%' flexDirection='row'>
+            <Image src={Ocean4} />
+            <Box>
+              <Text textTransform='capitalize' fontWeight='500' color='#B7CC33'>
+                Glass Curtains Wall
+              </Text>
+              <Text textTransform='capitalize' fontSize='16px' fontWeight='500'>
+                Coscharis Motors Lekki, Lagos
+              </Text>
+            </Box>
+          </Box>
+        </Box> */}
+      </Box>
       <Card
         Header='Reflecting our customers need.'
         text='Friendly, competitive & service driven'
